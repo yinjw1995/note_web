2dc008700062706f9d53612cdebed940351c4e8f
yinjw1995 <33319752+yinjw1995@users.noreply.github.com>
2025-11-09 21:59:22 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 import path from 'path'
 
 // https://vitejs.dev/config/
@@ -18,8 +20,8 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
+        tailwindcss,
+        autoprefixer,
       ],
     },
   },
